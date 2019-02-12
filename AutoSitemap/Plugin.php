f3d54de1adca4b583e7ec0b4a94c2ba6bcd336c1
Raymond Zhou <raymond9zhou@gmail.com>
2019-02-12 19:23:18 +0800

    Add files via upload

---
+++ AutoSitemap/Plugin.php
@@ -0,0 +1,54 @@
+<?php
+/**
+ * Auto generate sitemap plugin for typecho
+ * 
+ * @package AutoSitemap
+ * @author raymond9zhou
+ * @version 0.1.0
+ * @link https://ray8.cc
+ *
+ */
+class AutoSitemap_Plugin implements Typecho_Plugin_Interface
+{
+    /**
+     * 激活插件方法,如果激活失败,直接抛出异常
+     * 
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function activate(){
+	    Helper::addRoute('maproute', '/sitemap.xml', 'AutoSitemap_Action', 'action');
+    }
+    
+    /**
+     * 禁用插件方法,如果禁用失败,直接抛出异常
+     * 
+     * @static
+     * @access public
+     * @return void
+     * @throws Typecho_Plugin_Exception
+     */
+    public static function deactivate(){
+	    Helper::removeRoute('maproute');
+    }
+    
+    /**
+     * 获取插件配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form 配置面板
+     * @return void
+     */
+    public static function config(Typecho_Widget_Helper_Form $form){}
+    
+    /**
+     * 个人用户的配置面板
+     * 
+     * @access public
+     * @param Typecho_Widget_Helper_Form $form
+     * @return void
+     */
+    public static function personalConfig(Typecho_Widget_Helper_Form $form){}
+
+}
